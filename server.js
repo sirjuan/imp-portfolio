82d695ab3c6d767bf903f08935322409d192fa4e
Juha Pekkarinen <sirjuha@gmail.com>
2018-01-23 15:19:25 +0200

    using custom server

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.prepare()
   const server = express()
 
   server.get('/p/:id', (req, res) => {
-      const actualPage = '/post'
-      const queryParams = { id: req.params.id }
-      app.render(req, res, actualPage, queryParams)
+    const actualPage = '/post'
+    const queryParams = { title: req.params.id }
+    app.render(req, res, actualPage, queryParams)
   })
 
   server.get('*', (req, res) => {
