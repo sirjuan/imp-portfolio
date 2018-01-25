fb76b99241b9ae59d92672827432a605160f6895
sirjuan <t6peju01@students.oamk.fi>
2018-01-25 12:26:45 +0200

    Use Reactstrap for Layout and add PropTypes

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -1,16 +1,25 @@
+import PropTypes from 'prop-types'
+import { Container } from 'reactstrap'
 import Header from './Header'
+
 
 const layoutStyle = {
   margin: 20,
   padding: 20,
-  border: '1px solid #DDD'
+  border: '1px solid #DDD',
 }
 
-const Layout = (props) => (
-  <div style={layoutStyle}>
+const Layout = props => (
+  <div>
     <Header />
-    {props.children}
+    <Container>
+      {props.children}
+    </Container>
   </div>
 )
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
 
 export default Layout
