53e16165e86082f8b9d8cb017cc458b4a5939bae
sirjuan <t6peju01@students.oamk.fi>
2018-01-25 12:23:44 +0200

    Use markdown files to generate content. Use separate CSS file.

---
+++ markdown/post.md
@@ -0,0 +1,3 @@
+This is markdown text
+
+and more
--- pages/post.js
+++ pages/post.js
@@ -1,39 +1,26 @@
-import Layout from '../components/MyLayout.js'
+import PropTypes from 'prop-types'
 import Markdown from 'react-markdown'
+import Layout from '../components/MyLayout'
 
-export default (props) => (
+import content from './markdown/post'
+import styles from './post.css';
+
+const Post = props => (
   <Layout>
-   <h1>{props.url.query.title}</h1>
-   <div className="markdown">
-     <Markdown source={`
-This is our blog post.
-Yes. We can have a [link](/link).
-And we can have a title as well.
-
-### This is a title
-
-And here's the content.
-     `}/>
-   </div>
-   <style jsx global>{`
-     .markdown {
-       font-family: 'Arial';
-     }
-
-     .markdown a {
-       text-decoration: none;
-       color: blue;
-     }
-
-     .markdown a:hover {
-       opacity: 0.6;
-     }
-
-     .markdown h3 {
-       margin: 0;
-       padding: 0;
-       text-transform: uppercase;
-     }
-  `}</style>
+    <h1>{props.url.query.title}</h1>
+    <div className="markdown">
+      <Markdown source={content()} />
+    </div>
+    <style jsx>{styles}</style>
   </Layout>
 )
+
+Post.propTypes = {
+  url: PropTypes.shape({
+    query: PropTypes.shape({
+      title: PropTypes.string,
+    }),
+  }).isRequired,
+}
+
+export default Post
