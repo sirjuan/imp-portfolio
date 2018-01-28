770f08fceb8dc94ba88e885e1b1f88a737ae6072
sirjuan <t6peju01@students.oamk.fi>
2018-01-28 16:03:16 +0200

    sedfhfdgh

--- pages/post.js
+++ pages/post.js
@@ -2,18 +2,19 @@ import PropTypes from 'prop-types'
 import Markdown from 'react-markdown'
 import Layout from '../components/MyLayout'
 
-import content from './markdown/post' // eslint-disable-line
 import styles from './post.css'
 
-const Post = props => (
-  <Layout>
-    <h1>{props.url.query.title}</h1>
-    <div className="markdown">
-      <Markdown source={content()} />
-    </div>
-    <style jsx>{styles}</style>
-  </Layout>
-)
+const Post = (props) => {
+  const content = require('./markdown/post') // eslint-disable-line
+  return (
+    <Layout>
+      <h1>{props.url.query.title}</h1>
+      <div className="markdown">
+        <Markdown source={content()} />
+      </div>
+      <style jsx>{styles}</style>
+    </Layout>)
+}
 
 Post.propTypes = {
   url: PropTypes.shape({
